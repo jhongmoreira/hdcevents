df3f59d623de1d2d3bf197c059c86407b8a93d17
Jhonathan Moreira <arte.jhonathan@gmail.com>
2024-02-02 16:07:17 -0300

    Jstream

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,12 +30,19 @@
               <li class="nav-item">
                 <a href="/events/create" class="nav-link">Criar Eventos</a>
               </li>
+              @auth
               <li class="nav-item">
-                <a href="/" class="nav-link">Entrar</a>
+                <a href="/dashboard" class="nav-link">Meus Eventos</a>
+              </li>              
+              @endauth
+              @guest
+              <li class="nav-item">
+                <a href="/login" class="nav-link">Entrar</a>
               </li>
               <li class="nav-item">
-                <a href="/" class="nav-link">Cadastrar</a>
+                <a href="/register" class="nav-link">Cadastrar</a>
               </li>
+              @endguest
             </ul>
           </div>
         </nav>
