a74dbcb099e62f63189b6c8048af560cae28166a
Jhonathan Moreira <arte.jhonathan@gmail.com>
2024-02-02 08:44:07 -0300

    CSS pagina inicial

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,33 +18,27 @@
     </head>
     <body>  
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/">
-                        <img src="/img/hdcevents_logo.svg" alt="Logo da HDC Events">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">Eventos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/events/create">Criar Eventos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Entrar</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Cadastrar</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="collapse navbar-collapse" id="navbar">
+            <a href="/" class="navbar-brand">
+              <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+            </a>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a href="/" class="nav-link">Eventos</a>
+              </li>
+              <li class="nav-item">
+                <a href="/events/create" class="nav-link">Criar Eventos</a>
+              </li>
+              <li class="nav-item">
+                <a href="/" class="nav-link">Entrar</a>
+              </li>
+              <li class="nav-item">
+                <a href="/" class="nav-link">Cadastrar</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
         </header>
         <div class="container-fluid">
             @yield('content')
